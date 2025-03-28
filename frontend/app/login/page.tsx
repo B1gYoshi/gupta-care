f8e6e592dbb3bb80a95206e2e9ba23407ac5c489
Neel Joshi <neelsaliljoshi@gmail.com>
2025-03-28 00:33:45 -0400

    added base login components

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './login.css'
+
+export default function Login() {
 
 
-  return <div>
-      <h1>This is the login page</h1>
-  </div>;
+    return <div className='parent'>
+        <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        className='loginBox'
+        >
+			<TextField 
+				required 
+				id="outlined-email-input" 
+				label="email" 
+				variant="outlined" 
+			/>
+
+			<TextField
+				id="outlined-password-input"
+				required
+				label="password"
+				type="password"
+				autoComplete="current-password"
+			/>
+			<button>
+				Login
+			</button>
+        </Box>
+			
+    </div>
+    
 }
