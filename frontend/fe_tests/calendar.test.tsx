fc4bc0d6e167dd027078261aec3cc19d09c3e9cc
Neel Joshi <neelsaliljoshi@gmail.com>
2025-05-11 18:52:16 -0400

    added tests for records

--- frontend/fe_tests/calendar.test.tsx
+++ frontend/fe_tests/calendar.test.tsx
@@ -7,12 +7,12 @@ import { useRouter } from 'next/navigation';
 
 const mockedUseRouter = useRouter as jest.Mock;
 
-// Mock the next/router
+
 jest.mock('next/navigation', () => ({
     useRouter: jest.fn(),
   }));
 
-// Mock react-big-calendar since it's difficult to render in tests
+
 jest.mock('react-big-calendar', () => ({
   Calendar: ({ events, ...props }: any) => (
     <div data-testid="calendar-mock">Calendar Component</div>
@@ -44,11 +44,11 @@ describe('AppointmentCalendar Component', () => {
         fireEvent.click(createButton);
 
         await waitFor(() => {
-        // Check that modal inputs and button appear
-        expect(screen.getByLabelText(/appointment title/i)).toBeInTheDocument();
-        expect(screen.getByLabelText(/patient email/i)).toBeInTheDocument();
-        expect(screen.getByLabelText(/start date and time/i)).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /create appointment/i })).toBeInTheDocument();
+
+            expect(screen.getByLabelText(/appointment title/i)).toBeInTheDocument();
+            expect(screen.getByLabelText(/patient email/i)).toBeInTheDocument();
+            expect(screen.getByLabelText(/start date and time/i)).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: /create appointment/i })).toBeInTheDocument();
         });
     });
 });
